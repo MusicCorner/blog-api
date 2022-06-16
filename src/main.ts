5cee56dd9d4551c0735dec49414ea449d65451c0
Farshatov Alex <fla19991510@gmail.com>
2022-06-16 15:25:58 +0700

    husky test

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from '@app.module';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  console.log('test connction');
   app.listen(3000);
 };
 
