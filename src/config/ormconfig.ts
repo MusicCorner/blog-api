eff873ab42a832697c3f76a0e7dab0d500d29409
Farshatov Alex <fla19991510@gmail.com>
2022-07-07 17:23:42 +0700

    removed dev env file

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,4 +1,19 @@
+import path from 'path';
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+console.log(process.env.NODE_ENV);
+
+const pathName = path.resolve(
+  __dirname,
+  `../../dotenv/${process.env.NODE_ENV || 'development'}.env`
+);
+
+const env = config({
+  path: pathName,
+});
+
+console.log(env);
 
 export default new DataSource({
   type: 'postgres',
